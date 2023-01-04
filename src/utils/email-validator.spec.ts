348dde9bf2e9e920330592c08a688ead8403319d
Luiz Felipe M. Pereira <luiz_mst4@hotmail.com>
2023-01-03 22:40:20 -0300

    feat: ensure EmailValidatorAdapter call validator  with correct values

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
+
+  test('should call validator with correct values', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toBeCalledWith('any_email@mail.com')
+  })
 })
